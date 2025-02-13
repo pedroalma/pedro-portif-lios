882a844cf60f7e72a7eed106be9e932d9067b87f
pedroalma <pedroalma554@gmail.com>
2025-02-13 09:11:49 -0300

    novo

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//a funcao console.log() serve para mensagens por meio de
+//console, esta e util para debugar o codigo
+console.log("Sou uma mensagem de console");
+
+// //a funcao alert ela serve para exibir uma mensagem de alerta 
+// //ao usuario 
+// alert("sou uma mensagem de");
+// //a funcao prompt recebe uma informacao do usuario do usuario e armazena em 
+// //uma variavel se necessario 
+// prompt("qual o seu nome ?");
+
+//a declaracao de variavel let cria uma variavel de escopo local
+let nome = "pedro";
+//a declaracao de variavel var criada uma variavel de escopo global 
+var idade = 24;
+//a declaracao de variavel const cria uma variavel de escopo local,
+//porem que o valor nao pode ser alterado
+const cpf = '465.654.978-56'
